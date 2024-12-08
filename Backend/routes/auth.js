a11b05cc68897a7ad5bb26b55ab698fe1e29e7b5
shubham <shubha>
2024-12-09 00:29:10 +0530

    Adding the Authentication

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,12 +1,16 @@
 import express from 'express';
+import bcrypt from 'bcryptjs';
 import { body, validationResult } from 'express-validator';  // Importing body for validation
 import User from '../models/User.js';  // Ensure this is the correct path for your User model
+import jst from 'jsonwebtoken';
 
 const router = express.Router();
 
-// Define validation rules and POST route
+const JST_SECRET = 'Shubhamisagood$oy';
+
+// Route 1 : /api/auth/create create the user 
 router.post(
-  '/',
+  '/create',
   // Validate request body
   body('name').notEmpty().withMessage('Name is required'),
   body('email').isEmail().withMessage('Please provide a valid email').normalizeEmail(),
@@ -21,17 +25,27 @@ router.post(
 
     try {
       // If validation passes, create and save user
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password,salt);
+
       const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password, // In a real-world scenario, hash the password before saving
+        password: secPass, // In a real-world scenario, hash the password before saving
       });
-
+      const data = {
+        user : {
+          id:
+          user.id
+        }
+      }
       // Save the user
+      const authToken = jst.sign(data,JST_SECRET);
+
       await user.save();
-      res.status(201).json({ message: 'User created successfully', user });
+      res.status(201).json({authToken});
     } catch (err) {
-      // Handle duplicate email error or other issues
+      // Handle duplicate email error  or other issues
       if (err.code === 11000) { // MongoDB duplicate key error
         return res.status(400).json({ error: 'Email must be unique' });
       }
